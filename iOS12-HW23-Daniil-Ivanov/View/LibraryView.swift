4be83cde6a98f111b78f147364046f7124d49783
ivanovdale <dania-spb@mail.ru>
2024-05-11 16:50:45 +0300

    add library view template

---
+++ iOS12-HW23-Daniil-Ivanov/View/LibraryView.swift
@@ -0,0 +1,21 @@
+//
+//  LibraryView.swift
+//  AppleMusic
+//
+//  Created by Daniil (work) on 11.05.2024.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Медиатека")
+        }
+    }
+}
+
+#Preview {
+    LibraryView()
+}
