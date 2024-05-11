e2c51e0f3081be7e791d3d95c9fe3c02193ebe05
ivanovdale <dania-spb@mail.ru>
2024-05-11 15:10:27 +0300

    Initial Commit

---
+++ iOS12-HW23-Daniil-Ivanov/iOS12_HW23_Daniil_IvanovApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS12_HW23_Daniil_IvanovApp.swift
+//  iOS12-HW23-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS12_HW23_Daniil_IvanovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
