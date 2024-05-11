b5a0a9a695f463694900806a6b20178bddb1687f
ivanovdale <dania-spb@mail.ru>
2024-05-11 16:50:34 +0300

    add tab view in content view

---
+++ iOS12-HW23-Daniil-Ivanov/View/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  iOS12-HW23-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 11.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AppTabView()
+    }
+}
