eda7160fc5c60c2b0ce09ba4783754b1f1055612
ivanovdale <dania-spb@mail.ru>
2024-05-11 16:50:07 +0300

    implement app tab view

---
+++ iOS12-HW23-Daniil-Ivanov/View/AppTabView.swift
@@ -0,0 +1,72 @@
+//
+//  TabView.swift
+//  AppleMusic
+//
+//  Created by Daniil (work) on 11.05.2024.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    var body: some View {
+        TabView {
+            ForEach(Tab.allCases, content: tabView(_:))
+        }
+        .accentColor(.red)
+        .onAppear() {
+            let appearance = UITabBarAppearance()
+            appearance.shadowColor = .systemGray2
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+
+    private func tabView(_ tab: Tab) -> some View {
+        tab.content
+            .tabItem {
+                Image(systemName: tab.imageName)
+                Text(tab.title)
+            }
+    }
+}
+
+// MARK: - Tab values
+
+fileprivate enum Tab: CaseIterable {
+    case mediaLibrary, radio, search
+}
+
+// MARK: - Tab extension
+
+extension Tab: Identifiable {
+    var id: Self { self }
+}
+
+extension Tab {
+    var imageName: String {
+        switch self {
+        case .mediaLibrary: return "music.note.house.fill"
+        case .radio: return "dot.radiowaves.left.and.right"
+        case .search: return "magnifyingglass"
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .mediaLibrary: return "Медиатека"
+        case .radio: return "Радио"
+        case .search: return "Поиск"
+        }
+    }
+
+    // MARK: ViewBuilder
+
+    @ViewBuilder
+    var content: some View {
+        switch self {
+        case .mediaLibrary:
+            LibraryView()
+        case .radio, .search:
+            Text(self.title)
+        }
+    }
+}
