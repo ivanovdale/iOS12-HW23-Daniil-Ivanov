e0ccaa36de9962e413cf4555d2ce5462c1563837
ivanovdale <dania-spb@mail.ru>
2024-05-11 17:08:05 +0300

    implement library view

--- iOS12-HW23-Daniil-Ivanov/View/LibraryView.swift
+++ iOS12-HW23-Daniil-Ivanov/View/LibraryView.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 struct LibraryView: View {
     var body: some View {
-        NavigationView {
-            Text("Hello, World!")
+        GeometryReader { geometry in
+            NavigationView {
+                VStack(alignment: .center) {
+                    Text("Ищете свою музыку?")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Text("Здесь появится купленная Вами в iTunes Store музыка.")
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                        .padding(.horizontal, geometry.size.width * 0.1)
+                        .multilineTextAlignment(.center)
+                }
                 .navigationTitle("Медиатека")
+            }
         }
     }
 }
