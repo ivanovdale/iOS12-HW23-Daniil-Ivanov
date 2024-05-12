e6a7062bdfc298754cb36ccfb204b9a0a17d4b83
ivanovdale <dania-spb@mail.ru>
2024-05-12 20:13:57 +0300

    add category list view

---
+++ iOS12-HW23-Daniil-Ivanov/View/CategoryListView.swift
@@ -0,0 +1,50 @@
+//
+//  CategoryListView.swift
+//  AppleMusic
+//
+//  Created by Daniil (work) on 12.05.2024.
+//
+
+import SwiftUI
+
+struct CategoryListView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var categories = Category.allCases
+    @State private var editMode: EditMode = .active
+    @State private var multiSelection = Set<Category>()
+
+    var body: some View {
+        NavigationView {
+            List(selection: $multiSelection) {
+                ForEach(categories, id: \.id) { category in
+                    HStack {
+                        Image(systemName: category.imageName)
+                            .foregroundStyle(.red)
+                            .frame(width: 30)
+                        Text(category.description)
+                    }
+                    .listRowSeparator(.visible, edges: .all)
+                }
+                .onMove(perform: move)
+            }
+            .navigationTitle("Медиатека")
+            .toolbar {
+                Button("Готово") {
+                    dismiss()
+                }
+                .foregroundStyle(.red)
+                .fontWeight(.bold)
+            }
+            .listStyle(.grouped)
+            .scrollContentBackground(.hidden)
+            .environment(\.editMode, $editMode)
+            .tint(.red)
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+
+    private func move(from source: IndexSet, to destination: Int) {
+        categories.move(fromOffsets: source, toOffset: destination)
+    }
+}
+
