deb9149b2e5198e882906b90b1f52c9dda035aea
ivanovdale <dania-spb@mail.ru>
2024-05-31 11:45:14 +0300

    fix player offset

--- iOS12-HW23-Daniil-Ivanov/View/ContentView.swift
+++ iOS12-HW23-Daniil-Ivanov/View/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             AppTabView()
                 .safeAreaInset(edge: .bottom) {
                     PlayerView(height: geometry.size.height * 0.1,
-                               offset: 48)
+                               offset: 49)
                 }
         }
     }
